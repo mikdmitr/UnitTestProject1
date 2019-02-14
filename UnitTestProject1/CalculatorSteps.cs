ff967bf689dd596b93b5d8c4561c11ab532dcffe
mike <mikdmitr@gmail.com>
2019-02-14 23:46:24 +0300

    new features

--- UnitTestProject1/CalculatorSteps.cs
+++ UnitTestProject1/CalculatorSteps.cs
@@ -2,7 +2,8 @@
 using TechTalk.SpecFlow;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-
+using ClassLibrary3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProject1
 {
@@ -11,31 +12,39 @@ namespace UnitTestProject1
     [Binding]
     public class CalculatorSteps
     {
+        private int result;
+        private Calculator calculator = new Calculator();
+        //private int expectedResult;
+
         [Given(@"I have entered (.*) into the calculator")]
-        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
         {
-           
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "https://testqa.torrow.net";
-            ScenarioContext.Current.Pending();
+
+            //  IWebDriver driver = new ChromeDriver();
+            //  driver.Url = "https://testqa.torrow.net";
+            //  ScenarioContext.Current.Pending();
+            calculator.FirstNumber = number;
         }
         
         [Given(@"I have also entered (.*) into the calculator")]
-        public void GivenIHaveAlsoEnteredIntoTheCalculator(int p0)
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
         {
-            ScenarioContext.Current.Pending();
+            // ScenarioContext.Current.Pending();
+            calculator.SecondNumber = number;
         }
         
         [When(@"I press add")]
         public void WhenIPressAdd()
         {
-            ScenarioContext.Current.Pending();
+            //ScenarioContext.Current.Pending();
+            result = calculator.Add();
         }
         
         [Then(@"the result should be (.*) on the screen")]
-        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
         {
-            ScenarioContext.Current.Pending();
+            //ScenarioContext.Current.Pending();
+            Assert.AreEqual(expectedResult, result);
         }
     }
 }
---
+++ UnitTestProject1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary3
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
